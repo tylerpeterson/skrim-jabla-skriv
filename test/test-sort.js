eed408d4bf2a5e13a62b74e87d8f8a90d45a65e7
Tyler Peterson <petersontb@familysearch.org>
2013-10-03 17:17:40 -0600

    Dropping support for passing a single string for fields.
    Passing a single string for the sort field helped me shape the
    implementation, but it isn't required.
    If people are calling the function with a programmatic list then
    they'll always call it with a list, even if only one field is in it.
    If people are hard-coding a call with a single field then it seems a
    small trade-off to have them put it into brackets. You gain a
    consistent API.

--- test/test-sort.js
+++ test/test-sort.js
@@ -6,37 +6,33 @@ describe('sort', function () {
   it('should be a function', function () {
     expect(sort).to.be.a('function');
   });
-  describe('passing a single sort field', function () {
-    it('should sort by a single key', function () {
-      var list = [{name:'Zach'}, {name:'Alfred'}];
+  it('should sort by a single key', function () {
+    var list = [{name:'Zach'}, {name:'Alfred'}];
 
-      sort(list, 'name');
+    sort(list, ['name']);
 
-      expect(list[0].name).to.equal('Alfred');
-    });
-
-    it('should sort number values', function () {
-      var list = [{age: 55}, {age: 12}];
-
-      sort(list, 'age');
-
-      expect(list[0].age).to.equal(12);
-    });
+    expect(list[0].name).to.equal('Alfred');
   });
-  describe('passing multiple sort fields', function () {
-    it('should sort by leftmost field first', function () {
-      var list = [{name:'Zach', age:12}, {name:'Alfred', age:55}];
 
-      sort(list, ['name', 'age']);
+  it('should sort number values', function () {
+    var list = [{age: 55}, {age: 12}];
 
-      expect(list[0].name).to.equal('Alfred');
-    });
-    it('should sort by subsequent fields', function () {
-      var list = [{name:'John', age:19}, {name:'John', age:16}];
+    sort(list, ['age']);
 
-      sort(list, ['name', 'age']);
+    expect(list[0].age).to.equal(12);
+  });
+  it('should sort by leftmost field first', function () {
+    var list = [{name:'Zach', age:12}, {name:'Alfred', age:55}];
 
-      expect(list[0].age).to.equal(16);
-    });
+    sort(list, ['name', 'age']);
+
+    expect(list[0].name).to.equal('Alfred');
+  });
+  it('should sort by subsequent fields', function () {
+    var list = [{name:'John', age:19}, {name:'John', age:16}];
+
+    sort(list, ['name', 'age']);
+
+    expect(list[0].age).to.equal(16);
   });
 });
